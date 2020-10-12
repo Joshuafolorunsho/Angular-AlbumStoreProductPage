f8f378aa106d73d23ea8daa09852b71a2bb21e62
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-10-12 07:38:54 +0100

    wrap up part 3

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlbumStorePage } from '../../../e2e/app.po';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
+  albumInfo: AlbumStorePage;
 
-  constructor() { }
+  constructor(private _productService: ProductService) { 
+  }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
   }
 
 }
